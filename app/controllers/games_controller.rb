36411602ce1416805a97e0d029475779dcdeb9d2
Keita Wilson <dkwilson1991@gmail.com>
2022-11-07 20:52:32 +0900

    completed

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,28 @@
+require 'open-uri'
+
+class GamesController < ApplicationController
+  def new
+    @letters = []
+    10.times { @letters << (('A'..'Z').to_a.sample(1)) }
+    @letters
+  end
+
+  def score
+    @letters = params[:letters].split
+    @word = (params[:word] || '').upcase
+    @included = included?(@word, @letters)
+    @english_word = english_word?(@word)
+  end
+
+  private
+
+  def included?(word, letters)
+    word.chars.all? { |letter| word.count(letter) <= letters.count(letter) }
+  end
+
+  def english_word?(word)
+    response = URI.open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    json['found']
+  end
+end
